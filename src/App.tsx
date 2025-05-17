22199c813204c2aab6e0733c91fbc8a36430d1d3
sv1xx <fkftjduhd@gmail.com>
2025-05-17 21:24:02 +0700

    Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import SimpleReducer from "./components/simpleReducer";
+import { useTheme } from "./components/themeContext";
 
 function App() {
+  const { theme, toggleTheme } = useTheme();
   return (
-    <>
-      <div className="">
-        {/* <Treny /> */}
-        {/* <FetchedUsers /> */}
-        {/* <UserSearch /> */}
-        {/* <Todo /> */}
-        <SimpleReducer />
-      </div>
-    </>
+    <div
+      className={
+        theme === "dark" ? "bg-black text-white" : "bg-white text-black"
+      }
+    >
+      <h1>Текущая тема: {theme}</h1>
+      <button onClick={toggleTheme}>Сменить тему</button>
+      {/* <Treny /> */}
+      {/* <FetchedUsers /> */}
+      {/* <UserSearch /> */}
+      {/* <Todo /> */}
+      <SimpleReducer />
+    </div>
   );
 }
 
---
+++ src/components/themeContext.tsx
@@ -0,0 +1,41 @@
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  type ReactNode,
+} from "react";
+
+type Theme = "light" | "dark";
+
+type ThemeContextType = {
+  theme: Theme;
+  toggleTheme: () => void;
+};
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export const ThemeProvider = ({ children }: { children: ReactNode }) => {
+  const [theme, setTheme] = useState<Theme>("light");
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  };
+
+  useEffect(() => {
+    document.body.className = "";
+    document.body.classList.add(theme);
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (!context) throw new Error("Хук useTheme используется без Провайдера");
+  return context;
+};
